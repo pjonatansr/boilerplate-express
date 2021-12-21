775f632af41f05f84fa76a1ee3483c4aeccdebdd
Jonatan <johntechs.dev@gmail.com>
2021-12-21 10:42:08 +0000

    Learning about middlewares e routes

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ var app = express();
 
 console.log("Hello World");
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next();
+});
 
 app.get("/", (req, res) => {
   const absolutePath = __dirname + "/views/index.html";
@@ -15,12 +19,23 @@ app.get("/json", (req, res) => {
   const mySecret = process.env['MESSAGE_STYLE'];
 
   if (mySecret === "uppercase") {
-    res.send ({"message": "Hello json".toUpperCase()});
+    res.send({ "message": "Hello json".toUpperCase() });
   } else {
-    res.send( {"message": "Hello json"} );
+    res.send({ "message": "Hello json" });
   }
-
 });
+
+const getTime = () => new Date().toString();
+
+app.get('/now', function(req, res, next) {
+  req.time = getTime();
+  next();
+}, function(req, res) {
+  console.log(req.time);
+  res.send({time: req.time});
+});
+
+
 
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
