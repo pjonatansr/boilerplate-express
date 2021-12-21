84f0748be1c01fced83e95d517af90eb65d72b00
Jonatan <johntechs.dev@gmail.com>
2021-12-21 11:11:22 +0000

    Learning about REST in Express

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 console.log("Hello World");
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`)
@@ -19,9 +24,13 @@ app.get("/json", (req, res) => {
   const mySecret = process.env['MESSAGE_STYLE'];
 
   if (mySecret === "uppercase") {
-    res.send({ "message": "Hello json".toUpperCase() });
+    res.send(
+      { "message": "Hello json".toUpperCase() }
+    );
   } else {
-    res.send({ "message": "Hello json" });
+    res.send(
+      { "message": "Hello json" }
+    );
   }
 });
 
@@ -32,8 +41,36 @@ app.get('/now', function(req, res, next) {
   next();
 }, function(req, res) {
   console.log(req.time);
-  res.send({time: req.time});
+  res.send(
+    { time: req.time }
+  );
 });
+
+app.get("/:word/echo", (req, res) => {
+  res.send({
+    echo: req.params.word
+  });
+});
+
+app
+  .route("/name")
+  .get((req, res, next) => {
+    res.send(
+      {
+        name:
+          `${req.query.first} ${req.query.last}`
+      }
+    );
+    next();
+  })
+  .post((req, res) => {
+    res.send(
+      {
+        name:
+          `${req.body.first} ${req.body.last}`
+      }
+    )
+  });
 
 
 
