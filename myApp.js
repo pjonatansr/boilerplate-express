31e2fbf605ecdb0c41e607adc74fdcd736ef05f2
Jonatan <johntechs.dev@gmail.com>
2021-12-21 03:43:58 +0000

    add /json route and use process.env

--- myApp.js
+++ myApp.js
@@ -4,6 +4,26 @@ var app = express();
 console.log("Hello World");
 
 
+app.get("/", (req, res) => {
+  const absolutePath = __dirname + "/views/index.html";
+
+  res.sendFile(absolutePath);
+
+});
+
+app.get("/json", (req, res) => {
+  const mySecret = process.env['MESSAGE_STYLE'];
+
+  if (mySecret === "uppercase") {
+    res.send ({"message": "Hello json".toUpperCase()});
+  } else {
+    res.send( {"message": "Hello json"} );
+  }
+
+});
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
@@ -37,5 +57,4 @@ console.log("Hello World");
 
 
 
-
- module.exports = app;
+module.exports = app;
